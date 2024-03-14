86a826b0887d82d07e9353d63be5a21f3faafb40
robin hunter <rbnrggs@yahoo.com>
2024-03-13 16:00:12 -1000

    DefaultImage added with styling

--- src/components/ImageGenerator/index.jsx
+++ src/components/ImageGenerator/index.jsx
@@ -1,12 +1,16 @@
-import react from "react";
+import React from "react"; // This can be omitted if you're using React 17+ with the new JSX Transform and don't use React explicitly in the file.
 import "./style.css";
 
 const ImageGenerator = () => {
+    const defaultImage = 'https://raw.githubusercontent.com/robinahunter/AI-Image-Generator/main/src/assets/default-image-ai-generator.jpg';
+
     return (
-        <div>
-            
+        <div className="ai-image-generator">
+            <div className="header">Ai image <span>generator</span></div>
+            <div className="img-loading">
+                <div className="image"><img src={ defaultImage } alt="" /></div></div>
         </div>
-    )
+    );
 }
 
-export default ImageGenerator
+export default ImageGenerator;
