833227d5d6f0bfa5d3beb8edae566699df7c097d
robin hunter <rbnrggs@yahoo.com>
2024-03-13 14:48:19 -1000

    Vite React.js App configured and working

---
+++ vite.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import tailwindcss from 'tailwindcss';
+
+export default defineConfig({
+  plugins: [
+    reactRefresh(),
+    tailwindcss('./tailwind.config.js')
+  ]
+});
